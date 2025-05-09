daa48e18cdc98e4d4a5243602afb3f0bd2318998
GearShocky <gearshockmirasu@gmail.com>
2025-05-09 20:29:36 +0500

    wip setup

--- src/Hardman/Hardman.py
+++ src/Hardman/Hardman.py
@@ -1,21 +1,29 @@
-from moralis import sol_api
+import requests
 
-api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjI0NWYzNGVlLWM3OGItNGU0OS1iODM2LTg5YjZiZTAxZmM0ZSIsIm9yZ0lkIjoiNDQzMjg0IiwidXNlcklkIjoiNDU2MDgyIiwidHlwZUlkIjoiOTZmYmI2ZWItZmMzMC00ZTIwLThmYjItYjM2YmQ3NzgzODhiIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3NDUzNDE5NzEsImV4cCI6NDkwMTEwMTk3MX0.ftGHaLoitjs-mJuDftRUXOGYz1XjLiAkPxKU_cNONuU"
+api_key = "lwYRj6uiu8m5iExKumiFaZBh8Kf9fpOL"
+owner_address = "0x2Ee9a974FC5D0f38a531eDb01FaA76C3b72574b4"
 
+url = "https://eth-mainnet.g.alchemy.com/nft/v3/{}/getNFTs".format(api_key)
 params = {
-  "network": "devnet",
-  "address": "2W4z2NextyTDazZ6sd8SJ5DtCSuRbaaC816PhqiFZt7e"
+    "owner": owner_address,
+    "withMetadata": "true"
 }
 
-result = sol_api.account.get_nfts(
-  api_key=api_key,
-  params=params,
-)
-
-result2 = sol_api.account.balance(
-  api_key=api_key,
-  params=params,
-)
-
-print(result)
-print(result2)
+try:
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    
+    if response.status_code == 200:
+        data = response.json()
+        owned_nfts = data.get("ownedNfts", [])
+        if owned_nfts:
+            for nft in owned_nfts:
+                contract = nft["contract"]["address"]
+                token_id = nft["id"]["tokenId"]
+                print("Contract: {}, Token ID: {}".format(contract, int(token_id, 16)))
+        else:
+            print("No NFTs found for the given address.")
+    else:
+        print("Error: {} - {}".format(response.status_code, response.text))
+except requests.exceptions.RequestException as e:
+    print("Request failed: {}".format(e))
